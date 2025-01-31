f46f975c1c40c54c11d459f56ef9ec948a9d2673
sggenDebugs <inigopgonzalez@su.edu.ph>
2025-01-31 22:00:58 +0800

    enhance add_record.php; improve layout with header, footer, and dynamic field generation for better user experience

--- add_record.php
+++ add_record.php
@@ -44,31 +44,129 @@ $conn->close();
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/technology-icons.css">
+    <link rel="shortcut icon" type="image/png" href="img/favicon.png">
     <title>Add Record</title>
+    <style>
+    /* Style for the table */
+    .styled-table {
+        width: 100%;
+        border-collapse: collapse;
+        margin: 25px 0;
+        font-size: 0.9em;
+        font-family: sans-serif;
+        min-width: 400px;
+        box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+    }
+
+    .styled-table thead tr {
+        background-color: #009879;
+        color: #ffffff;
+        text-align: left;
+    }
+
+    .styled-table th,
+    .styled-table td {
+        padding: 12px 15px;
+    }
+
+    .styled-table tbody tr {
+        border-bottom: 1px solid #dddddd;
+    }
+
+    .styled-table tbody tr:nth-of-type(even) {
+        background-color: #f3f3f3;
+    }
+
+    .styled-table tbody tr:last-of-type {
+        border-bottom: 2px solid #009879;
+    }
+
+    .styled-table tbody tr:hover {
+        background-color: #f1f1f1;
+    }
+
+    /* Style for buttons */
+    .btn {
+        background-color: #009879;
+        color: white;
+        padding: 10px 20px;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+        font-size: 16px;
+        margin: 5px;
+    }
+
+    .btn:hover {
+        background-color: #007f63;
+    }
+    </style>
 </head>
+
 <body>
-    <h2>Add Record</h2>
-    <form method="post" action="add_record.php">
-        <label for="table">Table Name:</label>
-        <select name="table" id="table">
-            <option value="admins">Admins</option>
-            <option value="bikes">Bikes</option>
-            <option value="nfc_tags">NFC Tags</option>
-            <option value="transactions">Transactions</option>
-            <option value="users">Users</option>
-        </select><br><br>
-
-        <div id="fields">
-            <!-- Fields will be dynamically added here based on the selected table -->
+    <section class="banner">
+        <div class="bannerInside">
+            <img src="img/logo.png" center>
         </div>
+    </section>
+    <header>
+        <div id="header">
+            <a href="index.html" id="rdlogo"></a>
+            <nav>
+                <a href="#" id="menu_icon"></a>
+                <ul id="menu">
+                    <li><a href="index.php">Display Tables</a></li>
+                    <li><a href="add_record.php" class="current">Add Record</a></li>
+                    <li><a href="delete_record.php">Delete Record</a></li>
+                    <li><a href="#">Services</a>
+                        <ul class="hidden">
+                            <li><a href="3dprinting.html">3D Printing</a></li>
+                            <li><a href="design.html">PCB Design</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Tutorials</a>
+                        <ul class="hidden">
+                            <li><a href="qt.html">Qt C++</a></li>
+                            <li><a href="xhtml.html">XHTML</a></li>
+                            <li><a href="php.html">php</a></li>
+                            <li><a href="mysql.html">MySql</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="Contact.html">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
 
-        <button type="submit">Add Record</button>
-    </form>
+    <!-- Add Record Section -->
+    <section class="wrapper">
+        <h2>Add Record</h2>
+        <form method="post" action="add_record.php" class="form-style">
+            <label for="table">Table Name:</label>
+            <select name="table" id="table">
+                <option value="admins">Admins</option>
+                <option value="bikes">Bikes</option>
+                <option value="nfc_tags">NFC Tags</option>
+                <option value="transactions">Transactions</option>
+                <option value="users">Users</option>
+            </select><br><br>
 
-    <script>
+            <div id="fields">
+                <!-- Fields will be dynamically added here based on the selected table -->
+            </div>
+
+            <button type="submit" class="btn">Add Record</button>
+        </form>
+
+        <script>
         document.getElementById('table').addEventListener('change', function() {
             var table = this.value;
             var fieldsDiv = document.getElementById('fields');
@@ -99,6 +197,21 @@ $conn->close();
                 fieldsDiv.appendChild(document.createElement('br'));
             });
         });
-    </script>
+        </script>
+    </section>
+
+    <!-- Footer Section -->
+    <footer>
+        <ul class="socialMedia">
+            <li><a href="https://www.facebook.com" target="_blank"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="https://www.plus.google.com" target="_blank"><i class="fa fa-google-plus"></i></a></li>
+            <li><a href="https://www.twitter.com" target="_blank"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://www.youtube.com" target="_blank"><i class="fa fa-youtube"></i></a></li>
+        </ul>
+    </footer>
+    <footer class="last">
+        <p>&copy; IP</p>
+    </footer>
 </body>
+
 </html>
